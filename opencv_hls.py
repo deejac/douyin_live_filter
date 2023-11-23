82ef88f1a08577c2bc6241807cf72b30eabaec0c
deejac <deejac@qq.com>
2023-11-23 09:30:32 +0800

    自适应分辨率

--- opencv_hls.py
+++ opencv_hls.py
@@ -1,45 +1,47 @@
 import cv2
-import subprocess
+import ffmpeg
 
-# 设置输入视频流的URL
-input_url = "http://pull-hls-f6.douyincdn.com/media/stream-690588919695934124_md/index.m3u8"
-#input_url = "/Users/deejac/input.mp4"
-# 设置FFmpeg命令行参数
-ffmpeg_cmd = [
-    "ffmpeg",
-    "-f", "rawvideo",
-    "-pix_fmt", "bgr24",# 设置输入帧的像素格式
-    "-s", "640x360",  # 设置输入帧的尺寸
-    "-i", "-",  # 从标准输入读取帧数据
-    "-c:v", "libx264",
-    "-f", "hls",
-    "-hls_time", "2",  # 设置每个片段的时长（单位：秒）
-    "-hls_list_size", "5",  # 设置播放列表中的片段数
-    "output.m3u8"  # 输出的HLS文件名
-]
+# HLS协议视频源地址
+hls_url = "http://pull-hls-l6.douyincdn.com/third/stream-114132150640181287_md/index.m3u8?k=0d1e1c6dc0faf82f&t=1701307592"
 
-# 创建FFmpeg进程
-ffmpeg_process = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
+# 使用OpenCV读取HLS协议数据
+cap = cv2.VideoCapture(hls_url)
 
-# 创建OpenCV视频捕获对象
-cap = cv2.VideoCapture(input_url)
+# 获取输入源的分辨率
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+# 创建FFmpeg写入器
+output_filename = "output.m3u8"
+output = ffmpeg.output(
+    ffmpeg.input('pipe:', format='rawvideo', pix_fmt='bgr24', s='{}x{}'.format(width, height)),
+    output_filename,
+    vcodec='libx264',
+    preset='ultrafast',
+    tune='zerolatency',
+    f='hls',
+    hls_time=2,
+    hls_list_size=10,
+    hls_flags='delete_segments'
+)
+output = output.overwrite_output()
+
+# 打开FFmpeg进程
+process = ffmpeg.run_async(output, pipe_stdin=True)
+
+# 读取并处理每一帧数据
 while True:
-    # 读取一帧视频数据
     ret, frame = cap.read()
     if not ret:
         break
 
-    # 在这里对每帧数据进行卷积处理
-    # ...
+    # 在这里进行卷积处理，这里只是一个示例，你可以根据你的需求修改
+    #processed_frame = cv2.filter2D(frame, -1, kernel)
 
-    # 将处理后的帧数据写入FFmpeg的标准输入
-    cv2.imshow("frame", frame)
-    cv2.waitKey(1)
-    ffmpeg_process.stdin.write(frame.tostring())
-    #ffmpeg_process.stdin.write(frame.tobytes())
+    # 将处理后的帧数据写入FFmpeg进程的输入管道
+    process.stdin.write(frame.tobytes())
 
-# 关闭视频捕获对象和FFmpeg进程
+# 关闭FFmpeg进程和OpenCV视频流
+process.stdin.close()
+process.wait()
 cap.release()
-ffmpeg_process.stdin.close()
-ffmpeg_process.wait()
